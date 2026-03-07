a3bd9a8501182f08f8657bb2b26e395e2a47e418
aldialvayadi2 <aldialvayadi2@gmail.com>
2026-03-07 15:16:27 +0700

    Update Code and Dependency

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,23 @@
-FROM python:3.11-slim
+FROM python:3.14-slim
+
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
 
 WORKDIR /app
 
 COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
+
+RUN pip install --no-cache-dir --upgrade pip \
+    && pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
+RUN useradd -m appuser
+
+RUN chown -R appuser:appuser /app
+
+USER appuser
+
 EXPOSE 8080
 
-CMD ["python","main.py"]
+CMD ["gunicorn", "--bind", "0.0.0.0:8080", "app:app"]
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, request
-import os
-#import secrets
+from os import environ
+from subprocess import run
+import html
 
 app = Flask(__name__)
 
-#API_KEY = secrets.token_urlsafe(32)
-API_KEY = 'wwVE206wF2McwX3_BE8n0lKuMx5CxiMKPygcJjdt5XA'
+API_KEY = environ.get("API_KEY", "THIS_IS_API_KEY")
+
+ALLOWED_COMMANDS = {
+    "date": ["date"],
+    "uptime": ["uptime"],
+    "whoami": ["whoami"]
+}
 
 @app.route("/")
 def home():
@@ -17,7 +23,8 @@ def search():
     
     if request.method == "POST":
         q = request.form.get("q")
-        result = f"You searched for: {q}"
+        safe_q = html.escape(q)
+        result = f"You searched for: {safe_q}"
 
     return render_template("search.html", result=result)
 
@@ -32,7 +39,11 @@ def exec_cmd():
             return "Unauthorized", 401
         
         cmd = request.form.get("cmd")
-        output = os.popen(cmd).read()
+        output = run(
+            ALLOWED_COMMANDS[cmd],
+            capture_output=True,
+            text=True
+        )
 
     return render_template("exec.html", output=output)
 
